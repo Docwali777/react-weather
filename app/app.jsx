bd3b0e02168ef6488b145876fb2dcd7a9efd9059
Wali <drgauvin@mgmcare.org>
2017-02-05 23:04:31 -0500

    isuse with UDEMY and react-router lecture

--- app/app.jsx
+++ app/app.jsx
@@ -3,7 +3,6 @@ var ReactDOM = require("react-dom");
 var {Route, Router, IndexRoute, hashHistory} = require("react-router");
 var Main = require("Main");
 
-
 ReactDOM.render(<Router history={hashHistory}>
 <Route path="/" component={Main}>
 
