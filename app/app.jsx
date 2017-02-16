31977edfbd5d03b19e718b4c1b4623e8cec976c6
docwali777 <drgauvin@mgmcare.org>
2017-02-15 23:27:38 -0500

    prep for heroku

--- app/app.jsx
+++ app/app.jsx
@@ -2,9 +2,11 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 var {Route, Router, IndexRoute, hashHistory} = require("react-router");
 var Main = require("Main");
-var Weather = require("Weather")
+// var Weather = require("Weather")
+import Weather from "./components/Weather"
 var About = require("About")
-var Examples = require("Examples")
+// var Examples = require("Examples")
+import Examples from "./components/Examples"
 
 ReactDOM.render(
   <Router history={hashHistory}>
